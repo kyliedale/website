d47ddc5b7c1f8c47728b0cf338c8dd417ea057a4
Kylie Dale <kylie.dale@colorado.edu>
2016-10-20 13:51:54 -0600

    Style contact page

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,6 @@
 window.io = require('socket.io-client');
 
 document.addEventListener('DOMContentLoaded', function init() {
-  console.log($('#send'));
   $('#send').click(function() {
     console.log('in click');
   });
